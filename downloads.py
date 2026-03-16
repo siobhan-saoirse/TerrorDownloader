8914e5a8c36d9e08f84be5cb82c8ebf6f7db1a2c
Siobhan 🏳️‍⚧️ <33694155+siobhan-saoirse@users.noreply.github.com>
2026-03-16 19:20:14 -0400

    Update downloads.py

--- downloads.py
+++ downloads.py
@@ -8,7 +8,7 @@ def clone(endpath):
     run([vars.GIT_BINARY, 'config', '--global', 'http.maxRequestBuffer', '524288000'])
     run([vars.GIT_BINARY, 'config', '--global', 'core.compression', '9'])
     run([vars.GIT_BINARY, 'config', '--system', 'credential.helper', 'manager-core'])
-    run([vars.GIT_BINARY, 'clone', '--depth', '1', 'https://github.com/Lambdagon/fc', endpath], check=True)
+    run([vars.GIT_BINARY, 'clone', '--depth', '1', 'https://github.com/siobhan-saoirse/terror', endpath], check=True)
 
 def pull(endpath):
     run([vars.GIT_BINARY, 'config', '--global', 'http.postBuffer', '1048576000'])
